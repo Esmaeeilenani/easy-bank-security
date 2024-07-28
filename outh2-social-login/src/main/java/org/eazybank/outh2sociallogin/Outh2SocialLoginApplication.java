03a716a56ad5359ef0a154717c296d60ab86e703
esmaeeil enani <esmaeeilenani@gmail.com>
2024-07-28 15:41:02 +0300

    add oauth2 social login module

---
+++ outh2-social-login/src/main/java/org/eazybank/outh2sociallogin/Outh2SocialLoginApplication.java
@@ -0,0 +1,13 @@
+package org.eazybank.outh2sociallogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Outh2SocialLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Outh2SocialLoginApplication.class, args);
+	}
+
+}
---
+++ outh2-social-login/src/test/java/org/eazybank/outh2sociallogin/Outh2SocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eazybank.outh2sociallogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Outh2SocialLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
